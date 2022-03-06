ec45381069de392db3ae66e2d46c23fd2d6b0254
Piotr Przybylak <piotr.przybylak@codecool.com>
2022-03-06 10:20:57 +0100

    Implement sessions.

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 @RestController
 public class DemoController {
 
-    private final List<String> products = new ArrayList<>();
+    private final Random random = new Random();
+    private final Map<String, Map<String, Object>> sessions = new HashMap<>();
 
     @GetMapping("/")
     public String home() {
@@ -27,16 +29,37 @@ public class DemoController {
         return "Welcome to our secret page!";
     }
 
-    @GetMapping("/addtocart")
-    public String secret(String product) {
-        products.add(product);
-        System.out.println("You are adding product: " + product);
+
+    @GetMapping("/login")
+    public String login(HttpServletResponse response) {
+
+        // create a cookie
+        String sessionID = "" + random.nextInt();
+        Cookie cookie = new Cookie("super_secret_something", sessionID);
+        sessions.put(sessionID, new HashMap<>());
+
+        //add cookie to response
+        response.addCookie(cookie);
         return "Welcome to our secret page!";
     }
 
+
+    @GetMapping("/addtocart")
+    public String secret(String product, @CookieValue("super_secret_something") String id) {
+
+        Map<String, Object> session = sessions.get(id);
+
+        List<String> userProducts = (List<String>) session.getOrDefault("products", new ArrayList<>());
+        userProducts.add(product);
+        session.put("products", userProducts);
+
+
+        return "Thank you for you purchase! You bought: " + product;
+    }
+
     @GetMapping("/cart")
-    public List<String> cart() {
-        return products;
+    public List<String> cart(@CookieValue("super_secret_something") String id) {
+        return (List<String>) sessions.get(id).get("products");
     }
 
 }
