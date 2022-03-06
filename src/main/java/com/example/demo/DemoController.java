3c5e2a7fdf0563a83a2e4146e325eff6dcfcd2b2
Piotr Przybylak <piotr.przybylak@codecool.com>
2022-03-06 09:57:32 +0100

    Add global cart

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class DemoController {
 
+    private final List<String> products = new ArrayList<>();
 
     @GetMapping("/")
     public String home() {
@@ -21,6 +25,18 @@ public class DemoController {
     @GetMapping("/secret")
     public String secret() {
         return "Welcome to our secret page!";
+    }
+
+    @GetMapping("/addtocart")
+    public String secret(String product) {
+        products.add(product);
+        System.out.println("You are adding product: " + product);
+        return "Welcome to our secret page!";
+    }
+
+    @GetMapping("/cart")
+    public List<String> cart() {
+        return products;
     }
 
 }
